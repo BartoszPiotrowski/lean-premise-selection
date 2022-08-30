f8965ab2dc5850c67856eb7b513324cfc91648e1
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2022-08-30 18:18:09 +0100

    Fix tactic test

--- tests/TacticTest.lean
+++ tests/TacticTest.lean
@@ -1,4 +1,4 @@
-import LeanRandomForest.Tactic
+import PremiseSelection.Tactic
 
 example (a b c : Nat) (h : a < 4) : a + 0 = a := by {
   print_smt_features
@@ -10,4 +10,3 @@ example (a b c : Nat) : a + 0 = a := by {
   suggest_premises_with_scores
   apply rfl
 }
-
