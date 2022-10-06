e3a898d407f37dd10feae45d0ed042b5a6c315ff
E.W.Ayers <edward.ayers@outlook.com>
2022-10-06 16:39:10 +0100

    fix: StatementFeatures.subexpr should be a string

--- PremiseSelection/StatementFeatures.lean
+++ PremiseSelection/StatementFeatures.lean
@@ -50,7 +50,7 @@ structure StatementFeatures where
   /-- Just the constant's names and how frequently they arise. -/
   nameCounts : Multiset Name := ∅
   bigramCounts : Multiset Bigram := ∅
-  subexpressions : List Expr := ∅
+  subexpressions : Multiset String := ∅
 
 instance : ForIn M (Multiset α) (α × Nat) :=
   show ForIn _ (RBMap _ _ _) _ by infer_instance
@@ -69,8 +69,8 @@ def StatementFeatures.mkName : Name → StatementFeatures
 def StatementFeatures.mkBigram : Name → Name → StatementFeatures
   | n1, n2 => {bigramCounts := Multiset.singleton ⟨n1, n2⟩}
 
-def StatementFeatures.mkSubexpr : Expr → StatementFeatures
-  | x => {subexpressions := [x]}
+def StatementFeatures.mkSubexpr : String → StatementFeatures
+  | x => {subexpressions := Multiset.singleton x}
 
 def immediateName (e : Expr) : Option Name :=
   if let .const n _ := e then
@@ -84,7 +84,8 @@ def getHeadName? (e : Expr) : Option Name := do
   immediateName <| e.getAppFn
 
 def visitFeature (e : Expr) : WriterT StatementFeatures MetaM Unit  := do
-  tell <| StatementFeatures.mkSubexpr e
+  let ppe ←  Lean.PrettyPrinter.ppExpr e
+  tell <| StatementFeatures.mkSubexpr <| toString ppe
   if let some n := immediateName e then
     tell <| StatementFeatures.mkName n
   if e.isApp then
