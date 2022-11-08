16a24f34b329d436ed34c3d0a55af76932857789
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2022-11-08 16:43:54 +0000

    feat: args for recursive and user

--- Scripts/ExtractPremises.lean
+++ Scripts/ExtractPremises.lean
@@ -5,12 +5,14 @@ open Lean Lean.Meta
 
 open PremiseSelection
 
-set_option maxHeartbeats 10000000000
+set_option maxHeartbeats 100000000
 
 unsafe def main (args : List String) : IO Unit := do
   let labelsPath   := args.get! 0
   let featuresPath := args.get! 1
-  withImportModules [{ module := `Mathbin }] {} 0 fun env => do 
-    let m := extractPremisesFromImportsToFiles true true labelsPath featuresPath
+  let recursive    := args.get! 2 == "true"
+  let user         := args.get! 3 == "true"
+  withImportModules [{ module := `Mathbin.Algebra.Group.Defs }] {} 0 fun env => do 
+    let m := extractPremisesFromImportsToFiles recursive user labelsPath featuresPath
     let _ ← m.toIO { fileName := "", fileMap := default } { env := env }
     pure ()
