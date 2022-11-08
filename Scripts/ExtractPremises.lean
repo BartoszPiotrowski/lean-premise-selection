3c2f09202361a31bfb10f71e0d3ae85df905538c
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2022-11-08 15:24:11 +0000

    fix: extraction script does not take user input

--- Scripts/ExtractPremises.lean
+++ Scripts/ExtractPremises.lean
@@ -1,12 +1,6 @@
-import PremiseSelection
+import Mathbin
 
-open PremiseSelection
+import PremiseSelection.Extractor 
 
-def main (args : List String) : IO Unit := do
-  let labelsPath   := args.get! 0
-  let featuresPath := args.get! 1
-  
-  extractPremisesFromImportsToFiles (recursive := true) (user := true) labelsPath featuresPath
-
-  IO.println s!"Labels saved at {labelsPath}."
-  IO.println s!"Features saved at {featuresPath}."
+set_option maxHeartbeats 10000000000000 in
+extract_to_files l:"data/test.labels" f:"data/test.features"
