4a1613499dd3e15acb2ca55749e6fd70d70cf0df
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2022-12-27 18:25:44 +0100

    feat: max and min in any order

--- PremiseSelection/ExtractorRunner.lean
+++ PremiseSelection/ExtractorRunner.lean
@@ -4,7 +4,7 @@ open Lean Lean.Meta PremiseSelection
 
 /-- Run this file to extract premises as follows: 
   
-  `lean --run PremiseSelection/ExtractorRunner.lean data/test.labels data/test.features data/modules [max-depth=n] [+user] [+n] [+b] [+s]`
+  `lean --run PremiseSelection/ExtractorRunner.lean data/test.labels data/test.features data/modules [min-depth=x] [max-depth=y] [+user] [+n] [+b] [+s]`
 
 The first argument is the path to the labels file, the second argument is the 
 path to the features file, the third argument is the path to the modules file (a
@@ -28,21 +28,17 @@ unsafe def main (args : List String) : IO Unit := do
   let featuresPath    := args.get! 1
   let selectedModules := args.get! 2
 
-  -- Change the max depth allowed for proofs (recommended ~ 100).
-  let mut maxDepth : UInt32 := 255 
-  if 4 <= args.length && (args.get! 3).startsWith "max-depth=" then
-    let maxDepthStr := (args.get! 3).drop 10
-    let n := maxDepthStr.toNat!
-    if h : n < UInt32.size then
-      maxDepth := ⟨n, h⟩
-
-  -- Change the min depth allowed for proofs.
+  -- Change the min and max depth allowed for proofs (max recommended ~ 100).
   let mut minDepth : UInt32 := 0
-  if 5 <= args.length && (args.get! 4).startsWith "min-depth=" then
-    let minDepthStr := (args.get! 4).drop 10
-    let n := minDepthStr.toNat!
-    if h : n < UInt32.size then
-      minDepth := ⟨n, h⟩
+  let mut maxDepth : UInt32 := 0
+  for pos in [3, 4] do 
+    for key in ["min", "max"] do 
+      let fullKey := key ++ "-depth="
+      if pos + 1 <= args.length && (args.get! pos).startsWith fullKey then
+        let depthStr := (args.get! pos).drop fullKey.length
+        let n := depthStr.toNat!
+        if h : n < UInt32.size then
+          if key == "min" then minDepth := ⟨n, h⟩ else maxDepth := ⟨n, h⟩
 
   -- Add `+user` to the command to apply the user filter.
   let user := (args.drop 3).contains "+user"
