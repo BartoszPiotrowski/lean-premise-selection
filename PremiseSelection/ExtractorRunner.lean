b459c9542f92dfb966ceb8d436d4eb6becee20be
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2022-12-27 15:01:18 +0100

    chore: remove Mathbin import from runner

--- PremiseSelection/ExtractorRunner.lean
+++ PremiseSelection/ExtractorRunner.lean
@@ -1,7 +1,4 @@
-import Mathbin
 import PremiseSelection.Extractor 
-
-attribute [-instance] coeDecidableEq
 
 open Lean Lean.Meta PremiseSelection
 
@@ -33,9 +30,11 @@ unsafe def main (args : List String) : IO Unit := do
 
   -- Change the max depth allowed for proofs (recommended ~ 100).
   let mut maxDepth : UInt32 := 255 
-  if @LE.le Nat instLENat 4 args.length && (args.get! 3).startsWith "max-depth=" then
+  if LE.le 4 args.length && (args.get! 3).startsWith "max-depth=" then
     let maxDepthStr := (args.get! 3).drop 10
-    maxDepth := maxDepthStr.toNat!
+    let n := maxDepthStr.toNat!
+    if h : LT.lt n UInt32.size then
+      maxDepth := ⟨n, h⟩
 
   -- Add `+user` to the command to apply the user filter.
   let user := (args.drop 3).contains "+user"
