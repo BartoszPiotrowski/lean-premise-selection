99856f8e1c8aef76990b42d19c2fa85d8fd28bad
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2023-01-26 17:34:35 +0100

    fix: check only the last part of the name

--- PremiseSelection/ProofSource.lean
+++ PremiseSelection/ProofSource.lean
@@ -1,9 +1,9 @@
 import Lean
-import PremiseSelection.StatementFeatures 
+import PremiseSelection.StatementFeatures
 
 open Lean System
 
-namespace String 
+namespace String
 
 /-- Check if a string is contained in another one. -/
 partial def isSubstrOf (target str : String) : Bool :=
@@ -17,27 +17,27 @@ where
     else
       loop acc (str.next pos)
 
-end String 
+end String
 
 /-- Find file path from module imported from Mathbin. -/
-def pathFromMathbinImport (mod : Name) : MetaM (Option FilePath) := do 
+def pathFromMathbinImport (mod : Name) : MetaM (Option FilePath) := do
   let mathbinPath : System.FilePath := "." / "lake-packages" / "mathlib3port"
   SearchPath.findModuleWithExt [mathbinPath] "lean" mod
 
 /-- Find file path from module imported from Mathbin. -/
-def pathFromMathlibImport (mod : Name) : MetaM (Option FilePath) := do 
+def pathFromMathlibImport (mod : Name) : MetaM (Option FilePath) := do
   let mathbinPath : System.FilePath := "." / "lake-packages" / "mathlib"
   SearchPath.findModuleWithExt [mathbinPath] "lean" mod
 
 /-- Find file path of JSON with proof sources. -/
-def proofSourcePath (mod : Name) : MetaM (Option FilePath) := do 
+def proofSourcePath (mod : Name) : MetaM (Option FilePath) := do
   let mathbinPath : System.FilePath := "." / "data" / "proof_sources"
   SearchPath.findModuleWithExt [mathbinPath] "json" mod
 
 /-- Given a theorem name and a file path, extract the proof text. -/
 def proofSource (thm : Name) (json : Json) : MetaM (Option String) := do
-  if let Name.str _ thmStr := thm then 
-    match json.getObjVal? thmStr  with 
+  if let Name.str _ thmStr := thm then
+    match json.getObjVal? thmStr  with
     | Except.ok (Json.str s) =>
         return some s
     | _ => return none
@@ -45,34 +45,35 @@ def proofSource (thm : Name) (json : Json) : MetaM (Option String) := do
 
 namespace PremiseSelection
 
-/-- Given a list of premises and proof text, get rid of the ones that do not 
+/-- Given a list of premises and proof text, get rid of the ones that do not
 appear. We take into account `ToAdditive` name translations. -/
-def filterUserPremises (premises : Multiset Name) (proofSource : String) 
+def filterUserPremises (premises : Multiset Name) (proofSource : String)
   : Multiset Name := Id.run <| do
   let appearsInProof (s : String) : Bool := s.isSubstrOf proofSource
   let mut result := Std.RBMap.empty
-  for (p, c) in premises do 
-    if appearsInProof p.toString then
+  for (p, c) in premises do
+    let pLast := (Syntax.splitNameLit p.toString.toSubstring).reverse.head!.toString
+    if appearsInProof pLast then
       result := result.insert p c
   return result
 
-/-- Like `filterUserPremises` but simply checks that the premise appears 
+/-- Like `filterUserPremises` but simply checks that the premise appears
 somewhere in the file, instead of looking for the proof source. -/
-def filterUserPremisesFromFile 
+def filterUserPremisesFromFile
   (premises : Multiset Name) (referencePath : FilePath)
-  : IO (Multiset Name) := do 
-  let appearsInFile (s : String) : IO Bool := do 
+  : IO (Multiset Name) := do
+  let appearsInFile (s : String) : IO Bool := do
     let args := #[s, referencePath.toString]
     let output ← IO.Process.output { cmd := "grep", args := args }
-    if output.exitCode != 0 then 
+    if output.exitCode != 0 then
       return false
-    if output.stdout.isEmpty then 
+    if output.stdout.isEmpty then
       return false
     return true
   let mut result := Std.RBMap.empty
-  for (p, c) in premises do 
-    if ← appearsInFile p.toString then 
+  for (p, c) in premises do
+    if ← appearsInFile p.toString then
       result := result.insert p c
-  return result  
+  return result
 
 end PremiseSelection
