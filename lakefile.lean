6f18f5d240be6a6e2ea2fb198f85fafbe763bb96
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2023-05-19 18:24:46 +0200

    revert to old mathlib3port (couldn't compile it)

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,9 @@ open Lake DSL
 
 package leanPremiseSelection
 
-require mathlib3port from git "https://github.com/leanprover-community/mathlib3port.git"@"65ed2fb26d6ba05daf18072566b2a2c72f5cd370"
+-- require mathlib3port from git "https://github.com/leanprover-community/mathlib3port.git"@"65ed2fb26d6ba05daf18072566b2a2c72f5cd370"
+
+require mathlib3port from git "https://github.com/leanprover-community/mathlib3port.git"@"f4e5dfe2aa778b4cc42620b6b58442504348d20d"
 
 @[default_target]
 lean_lib PremiseSelection
