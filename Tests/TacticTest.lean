9476dab2fcf6ee908ecc3ec0039b578cff4840ce
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2023-05-22 23:00:07 +0200

    no widget

--- Tests/TacticTest.lean
+++ Tests/TacticTest.lean
@@ -7,20 +7,3 @@ open PremiseSelection
 variable {M : Type u} [RightCancelMonoid M] {a b : M}
 variable [CommSemigroup G]
 
-example : b = a * b ↔ a = 1 := by {
-  rw [eq_comm]
-  suggest_premises
-  sorry
-}
-
-example : ∀ a b c : G, a * (b * c) = b * (a * c) := by
-{
-  intros a b c
-  suggest_premises
-  sorry
-}
-
-example (a : Nat) : 0 + a = a := by {
-  suggest_premises
-  sorry
-}
