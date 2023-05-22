c165bd7d46e16946210f27cbe0de1b4cba17adbc
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2023-05-22 22:56:16 +0200

    no widget

--- lakefile.lean
+++ lakefile.lean
@@ -24,35 +24,3 @@ lean_exe Predict where
 lean_exe KnnPredict where
   root := `Scripts.KnnPredict
 
-def npmCmd : String := "npm"
-
-target packageLock : FilePath := do
-  let widgetDir := __dir__ / "widget"
-  let packageFile ← inputFile <| widgetDir / "package.json"
-  let packageLockFile := widgetDir / "package-lock.json"
-  buildFileAfterDep packageLockFile packageFile fun _srcFile => do
-    proc {
-      cmd := npmCmd
-      args := #["install"]
-      cwd := some widgetDir
-    }
-
-def tsxTarget (pkg : Package) (tsxName : String) [PackageName pkg _package.name]
-    : IndexBuildM (BuildJob FilePath) := do
-  let widgetDir := __dir__ / "widget"
-  let jsFile := widgetDir / "dist" / s!"{tsxName}.js"
-  let deps : Array (BuildJob FilePath) := #[
-    ← inputFile <| widgetDir / "src" / s!"{tsxName}.tsx",
-    ← inputFile <| widgetDir / "rollup.config.js",
-    ← inputFile <| widgetDir / "tsconfig.json",
-    ← fetch (pkg.target ``packageLock)
-  ]
-  buildFileAfterDepArray jsFile deps fun _srcFile => do
-    proc {
-      cmd := npmCmd
-      args := #["run", "build", "--", "--tsxName", tsxName]
-      cwd := some widgetDir
-    }
-
-@[default_target]
-target widgetJs (pkg : Package) : FilePath := tsxTarget pkg "index"
