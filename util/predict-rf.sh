0dc5b2d1a9cc018fdf98c6b0720da6bde7629e1c
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2023-05-23 17:57:10 +0200

    predict only

---
+++ util/predict-rf.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+DIR=`pwd`
+TEST_FEATURES=$1
+TEST_LABELS=$2
+FOREST=$3
+LOG=${0%.*}.`basename $TEST_FEATURES`.`basename $FOREST`.$RANDOM.log
+PREDS=${LOG%.*}.preds
+
+RF_DIR=../lean-premise-selection
+PREDICT_BINARY=$RF_DIR/build/bin/Predict
+cd $RF_DIR && lake build && cd $DIR &&
+echo "Log being written to $LOG" &&
+echo "Evaluating on $TEST_FEATURES" &&
+{
+    time ./$PREDICT_BINARY \
+    $FOREST \
+    $TEST_FEATURES \
+    $PREDS
+} 2>&1 | tee -a $LOG &&
+echo
+./util/cover.py $TEST_LABELS $PREDS --n 0 | tee -a $LOG &&
+./util/cover.py $TEST_LABELS $PREDS --n 10 | tee -a $LOG
