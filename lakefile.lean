4daee8d0f3803843b2989594ec28bbf9c35a105a
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2023-05-29 14:04:03 +0200

    older version of mathlib3port

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,8 @@ open Lake DSL
 
 package leanPremiseSelection
 
-require mathlib3port from git "https://github.com/leanprover-community/mathlib3port.git"@"7a29664099a8edda5cee335f763b5598626b26c8"
+-- require mathlib3port from git "https://github.com/leanprover-community/mathlib3port.git"@"65ed2fb26d6ba05daf18072566b2a2c72f5cd370"
+require mathlib3port from git "https://github.com/leanprover-community/mathlib3port.git"@"f4e5dfe2aa778b4cc42620b6b58442504348d20d"
 
 @[default_target]
 lean_lib PremiseSelection
@@ -37,7 +38,7 @@ target packageLock : FilePath := do
       cwd := some widgetDir
     }
 
-def tsxTarget (pkg : Package) (tsxName : String) [PackageName pkg _package.name]
+def tsxTarget (pkg : Package) (tsxName : String) [Fact (pkg.name = _package.name)]
     : IndexBuildM (BuildJob FilePath) := do
   let widgetDir := __dir__ / "widget"
   let jsFile := widgetDir / "dist" / s!"{tsxName}.js"
