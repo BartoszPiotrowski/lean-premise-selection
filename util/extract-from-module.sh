514dab2d3d9eecac5f11a97f0a04f917b0520a17
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-11-05 18:29:51 +0000

    fix: add proof widgets to LEAN_PATH

--- util/extract-from-module.sh
+++ util/extract-from-module.sh
@@ -12,6 +12,7 @@ export LEAN_PATH=$LEAN_PATH:lake-packages/mathlib/build/lib
 export LEAN_PATH=$LEAN_PATH:lake-packages/std/build/lib
 export LEAN_PATH=$LEAN_PATH:lake-packages/Qq/build/lib
 export LEAN_PATH=$LEAN_PATH:lake-packages/aesop/build/lib
+export LEAN_PATH=$LEAN_PATH:lake-packages/proofwidgets/build/lib
 
 lean --run --memory=4096 --timeout=100000000000 \
     $LEAN_EXTRACTOR \
