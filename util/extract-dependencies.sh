3999ced6e563cfc150dee70ae0fdae4b7f5ea652
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2023-11-10 12:00:52 +0100

    fix: paste removed, seemed to be unnecessary

--- util/extract-dependencies.sh
+++ util/extract-dependencies.sh
@@ -1,7 +1,9 @@
-MATHBIN=lake-packages/mathlib3port/Mathbin
-find $MATHBIN -name '*.lean' ! -name "All.lean" | while read f; do
-    { module=`echo $f | sed 's/.*Mathbin/Mathbin/g; s/.lean$//g; s/\//./g'`
-    deps=`grep '^import Math' $f | sed 's/import //g' | paste -s -d' '`
+#!/bin/bash
+MATHLIB=lake-packages/mathlib/Mathlib
+find $MATHLIB -name '*.lean' ! -name "Mathlib.lean" | while read f; do
+    {
+    module=`echo $f | sed 's/.*Mathlib/Mathlib/g; s/.lean$//g; s/\//./g'`
+    deps=`grep '^import Math' $f | sed 's/import //g'`
     echo $module:$deps
     } &
 done
