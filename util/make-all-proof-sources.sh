f7829e6f6e2da6303d09a15c64ef9113f82947ce
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-11-10 18:37:03 +0000

    wip: parallelizing proof source maker

--- util/make-all-proof-sources.sh
+++ util/make-all-proof-sources.sh
@@ -4,8 +4,29 @@ MATHLIB=lake-packages/mathlib/Mathlib
 
 SCRIPT_DIR=`dirname $0`
 
+MODULES_TO_PROCESS=()
+
 find $MATHLIB -name '*.lean' ! -name "Mathlib.lean" | while read f; do
     module=`echo $f | sed 's/.*Mathlib/Mathlib/g; s/.lean$//g; s/\//./g'`
-    echo "Making proof sources for $module"
-    $SCRIPT_DIR/make-proof-sources.sh $module
+    MODULES_TO_PROCESS+=("$module")
+
+    if [ ${#MODULES_TO_PROCESS[@]} -eq 8 ]; then
+        (
+            for module in "${MODULES_TO_PROCESS[@]}"; do
+                $SCRIPT_DIR/make-proof-sources.sh "$module" &
+            done
+            wait
+        )
+        MODULES_TO_PROCESS=()
+    fi
 done
+
+# Run the remaining modules (if any) outside the loop
+if [ ${#MODULES_TO_PROCESS[@]} -gt 0 ]; then
+    (
+        for module in "${MODULES_TO_PROCESS[@]}"; do
+            $SCRIPT_DIR/make-proof-sources.sh "$module" &
+        done
+        wait
+    )
+fi
