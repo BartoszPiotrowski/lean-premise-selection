2ed6eb056308c64c8e27a8fba346ff57ebd2d4d1
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-11-11 16:18:11 +0000

    feat: extract using all_modules.txt

--- util/extract-from-all-modules.sh
+++ util/extract-from-all-modules.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 #
-MATHLIB=lake-packages/mathlib/Mathlib
-
 PARAMS=$@
-PARAMS_NAME=`echo $PARAMS | sed 's/ /./g'`
 OUT_DIR=data/extracted.$PARAMS_NAME
 SCRIPT_DIR=`dirname $0`
+
 mkdir $OUT_DIR
 
-# Make proof sources.
-if [ ! -d data/proof_sources ]; then
-    $SCRIPT_DIR/make-all-proof-sources.sh
-fi
-
-find $MATHLIB -name '*.lean' ! -name "Mathlib.lean" | while read f; do
+while read f; do
     module=`echo $f | sed 's/.*Mathlib/Mathlib/g; s/.lean$//g; s/\//./g'`
     echo "Extracting from $module"
     $SCRIPT_DIR/extract-from-module.sh $module $OUT_DIR $PARAMS
-done
+done < "$SCRIPT_DIR/all_modules.txt";
