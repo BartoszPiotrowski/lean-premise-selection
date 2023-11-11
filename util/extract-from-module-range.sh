f4985d0e08a380827385e07dcc361f1715a29038
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-11-11 17:35:49 +0000

    fix: params in extraction script

--- util/extract-from-module-range.sh
+++ util/extract-from-module-range.sh
@@ -2,12 +2,16 @@
 #
 FROM=$1
 TO=$2
+PARAMS="+source +n +b"
+PARAMS_NAME=`echo $PARAMS | sed 's/ /./g'`
 OUT_DIR=data/extracted.$PARAMS_NAME
 SCRIPT_DIR=`dirname $0`
 
 if [ ! -d $OUT_DIR ]; then
     mkdir $OUT_DIR
 fi
+
+MODULES_TO_PROCESS=()
 I=0
 
 # NOTE: parameters are fixed to `+source +n +b` here.
@@ -15,7 +19,7 @@ while read f; do
     if [ $I -ge $FROM ] && [ $I -le $TO ]; then
         module=`echo $f | sed 's/.*Mathlib/Mathlib/g; s/.lean$//g; s/\//./g'`
         echo "Extracting from $module"
-        $SCRIPT_DIR/extract-from-module.sh $module $OUT_DIR +source +n +b
+        $SCRIPT_DIR/extract-from-module.sh $module $OUT_DIR $PARAMS
     fi;
     I=$((I+1))
 done < "$SCRIPT_DIR/all_modules.txt";
