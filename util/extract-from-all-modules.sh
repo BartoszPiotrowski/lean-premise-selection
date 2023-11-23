30f0f559ed31ce8e6d4e7fe52d49ad1ec33ffefa
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2023-11-23 16:48:16 +0100

    don't rely on util/all_modules.txt

--- util/extract-from-all-modules.sh
+++ util/extract-from-all-modules.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-#
+
 PARAMS=$@
+PARAMS_NAME=`echo $PARAMS | sed 's/ /./g'`
 OUT_DIR=data/extracted.$PARAMS_NAME
 SCRIPT_DIR=`dirname $0`
+MATHLIB=lake-packages/mathlib/Mathlib
 
 mkdir $OUT_DIR
 
-while read f; do
+find $MATHLIB -name '*.lean' ! -name "All.lean" | while read f; do
+    echo $f
     module=`echo $f | sed 's/.*Mathlib/Mathlib/g; s/.lean$//g; s/\//./g'`
     echo "Extracting from $module"
     $SCRIPT_DIR/extract-from-module.sh $module $OUT_DIR $PARAMS
-done < "$SCRIPT_DIR/all_modules.txt";
+done
